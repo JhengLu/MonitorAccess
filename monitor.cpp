7065cd3f700dfdd5a1690e4ed2437280e91b192d
Jiaheng Lu <77672985+JhengLu@users.noreply.github.com>
2023-12-18 20:27:49 -0500

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -1048,7 +1048,7 @@ int main (int argc, char *argv[]) {
     //monitor.measure_process_latency("memcached");
 
     std::set<int> node1_cores = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61};
-    monitor.measure_cores_latency(node1_cores);
+    //monitor.measure_cores_latency(node1_cores);
 
     //ApplicationInfo *app_info_1 = new ApplicationInfo("test_page_freq");
     //monitor.add_application(app_info_1);
